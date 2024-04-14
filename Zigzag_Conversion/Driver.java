927f76a3581565d827d87107853da72e1754a0c4
Huangming Wang <75608391+Risingvalley@users.noreply.github.com>
2024-04-14 12:13:45 -0400

    Add files via upload

---
+++ Zigzag_Conversion/Driver.java
@@ -0,0 +1,9 @@
+package Zigzag_Conversion;
+
+public class Driver {
+    public static void main(String[] args) {
+        String s="PAYPALISHIRING";
+        Solution test=new Solution();
+        System.out.println(test.convert(s, 3));
+    }
+}
---
+++ Zigzag_Conversion/Solution1.java
@@ -0,0 +1,70 @@
+package Zigzag_Conversion;
+
+public class Solution1 {
+    //convert the string as N shape
+    public String convert(String s, int numRows){
+        if(numRows==1){
+            return s;
+        }
+        int a1=numRows*2-2;     //nums of char for each A group
+        int a2=s.length()/a1;   //nums of group
+        int a3=s.length()%a1;   //remain characters
+        int a4=numRows-1;       //column of each group
+        int column=a4*a2;       //the nums of column we need
+        if(a3!=0){
+            column++;           //we need one more column for those remain characters
+            if(a3>numRows){
+                column=column+(a3-numRows);     //need one extra column for each remain character if greater than numRows
+            }
+        }
+        column++;               //one more column for "\n"
+        //make a char array and make all positions contain space
+        char[] output=new char[column*numRows];
+        for(int i=0;i<output.length;i++){
+            output[i]=' ';
+        }
+
+        //add "\n" for the end of each row
+        for(int i=1;i<numRows;i++){
+            output[i*(column)-1]='\n';
+        }
+
+        //put the characters in to output
+        int currCol=0;
+        int currRow=0;
+        int currCharater=0;
+        int flag=0;     //0 means go down ward, 1 means go up ward
+        
+        //fill in the chracter to the char[]
+        while(currCharater<s.length()){
+            switch (flag) {
+                case 0:
+                    while (currRow<numRows && currCharater<s.length()) {
+                        output[currCol+currRow*column]=s.charAt(currCharater);
+                        currCharater++;
+                        currRow++;
+                    }
+                    flag=1;
+                    currCol++;
+                    currRow=currRow-2;
+                    break;
+                case 1:
+                    while (currRow>0 && currCharater<s.length()) {
+                        output[currCol+currRow*column]=s.charAt(currCharater);
+                        currCharater++;
+                        currCol++;
+                        currRow--;
+                    }
+                    flag=0;
+                default:
+                    break;
+            }
+        }
+        //make the char[] to a String
+        StringBuilder builder=new StringBuilder(column*numRows);
+        for(int z=0;z<output.length;z++){
+            builder.append(output[z]);
+        }
+        return builder.toString();
+    }
+}
